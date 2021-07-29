8d25bb89123a91724f03be377ce04645050d8f70
rizalaji1st <rizalaji561@gmail.com>
2021-07-29 15:39:40 +0700

    Mengubah default driver storage ke minio

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'default' => env('FILESYSTEM_DRIVER', 'minio'),
 
     /*
     |--------------------------------------------------------------------------
@@ -50,6 +50,16 @@ return [
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
+        ],
+
+        'minio' => [
+            'driver' => 's3',
+            'endpoint' => env('MINIO_ENDPOINT', 'https://cdnb.uns.ac.id'),
+            'use_path_style_endpoint' => true,
+            'key'   => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET')
         ],
 
     ],
