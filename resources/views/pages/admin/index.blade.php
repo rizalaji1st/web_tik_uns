04a34d59fb95d088a15df7ba746e3e3ca4432bc3
rizalaji1st <rizalaji561@gmail.com>
2021-07-29 15:12:15 +0700

    Mengubah index dashboard admin

--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -7,7 +7,7 @@
     <!-- Default box -->
     <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Hooray</h3>
+          <h3 class="card-title">Selamat datang {{$user->name}}</h3>
 
           <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
@@ -19,13 +19,9 @@
           </div>
         </div>
         <div class="card-body">
-          adminLTE udah include gais, selamat lanjut ngodonf
+          Selamat datang di dashboard admin web TIK UNS
         </div>
         <!-- /.card-body -->
-        <div class="card-footer">
-          ini cuma Footer
-        </div>
-        <!-- /.card-footer-->
       </div>
       <!-- /.card -->
 @endsection
